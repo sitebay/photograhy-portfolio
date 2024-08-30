77be1d8feabc3f3339fce48740d7c2ae3aa7e4ed
sitebay <support@sitebay.org>
2024-08-30 00:05:59 +0000

    saving

--- docs/wp-login.php
+++ docs/wp-login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html><html lang="en-US"><head>
 	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
-	<title>Log In ‹ Photography — WordPress</title>
+	<title>Lost Password ‹ Photography — WordPress</title>
 	<meta content="max-image-preview:large, noindex, noarchive" name="robots">
 <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect">
 <link href="wp-includes/css/dashicons.min.css__querystring__ver=6.6.1" id="dashicons-css" media="all" rel="stylesheet" type="text/css">
@@ -11,7 +11,7 @@
 	<meta content="strict-origin-when-cross-origin" name="referrer">
 		<meta content="width=device-width" name="viewport">
 		<script src="https://my.sitebay.org/static/sitebay.js"></script><script>!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]); posthog.init("sTMFPsFhdP1Ssg",{api_host:"https://my.sitebay.org"})</script></head>
-	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-en-us">
 	<script type="text/javascript">
 /* <![CDATA[ */
 document.body.className = document.body.className.replace('no-js','js');
@@ -20,76 +20,33 @@ document.body.className = document.body.className.replace('no-js','js');
 
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
-	
-		<form action="https://mytemplatesite.sitebay.ca/wp-login.php" id="loginform" method="post" name="loginform">
+	<div class="notice notice-info message"><p>Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p></div>
+
+		<form action="https://mytemplatesite.sitebay.ca/wp-login.php?action=lostpassword" id="lostpasswordform" method="post" name="lostpasswordform">
 			<p>
 				<label for="user_login">Username or Email Address</label>
-				<input autocapitalize="off" autocomplete="username" class="input" id="user_login" name="log" required="required" size="20" type="text" value="">
+				<input autocapitalize="off" autocomplete="username" class="input" id="user_login" name="user_login" required="required" size="20" type="text" value="">
 			</p>
-
-			<div class="user-pass-wrap">
-				<label for="user_pass">Password</label>
-				<div class="wp-pwd">
-					<input autocomplete="current-password" class="input password-input" id="user_pass" name="pwd" required="required" size="20" spellcheck="false" type="password" value="">
-					<button aria-label="Show password" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" type="button">
-						<span aria-hidden="true" class="dashicons dashicons-visibility"></span>
-					</button>
-				</div>
-			</div>
-						<p class="forgetmenot"><input id="rememberme" name="rememberme" type="checkbox" value="forever"> <label for="rememberme">Remember Me</label></p>
+						<input name="redirect_to" type="hidden" value="">
 			<p class="submit">
-				<input class="button button-primary button-large" id="wp-submit" name="wp-submit" type="submit" value="Log In">
-									<input name="redirect_to" type="hidden" value="https://mytemplatesite.sitebay.ca/wp-admin/">
-									<input name="testcookie" type="hidden" value="1">
+				<input class="button button-primary button-large" id="wp-submit" name="wp-submit" type="submit" value="Get New Password">
 			</p>
 		</form>
 
-					<p id="nav">
-				<a class="wp-login-lost-password" href="wp-login.php__querystring__action=lostpassword">Lost your password?</a>			</p>
-			<script type="text/javascript">
-/* <![CDATA[ */
-function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
-wp_attempt_focus();
-if ( typeof wpOnload === 'function' ) { wpOnload() }
-/* ]]> */
-</script>
-		<p id="backtoblog">
+		<p id="nav">
+			<a class="wp-login-log-in" href="wp-login.php">Log in</a>
+					</p>
+				<p id="backtoblog">
 			<a href="index.html">← Go to Photography</a>		</p>
 			</div>
-			<script id="jquery-core-js" src="wp-includes/js/jquery/jquery.min.js__querystring__ver=3.7.1" type="text/javascript"></script>
-<script id="jquery-migrate-js" src="wp-includes/js/jquery/jquery-migrate.min.js__querystring__ver=3.4.1" type="text/javascript"></script>
-<script id="zxcvbn-async-js-extra" type="text/javascript">
+			<script type="text/javascript">
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"https:\/\/mytemplatesite.sitebay.ca\/wp-includes\/js\/zxcvbn.min.js"};
+
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		
 /* ]]> */
 </script>
-<script id="zxcvbn-async-js" src="wp-includes/js/zxcvbn-async.min.js__querystring__ver=1.0" type="text/javascript"></script>
-<script id="wp-hooks-js" src="wp-includes/js/dist/hooks.min.js__querystring__ver=2810c76e705dd1a53b18" type="text/javascript"></script>
-<script id="wp-i18n-js" src="wp-includes/js/dist/i18n.min.js__querystring__ver=5e580eb46a90c2b997e6" type="text/javascript"></script>
-<script id="wp-i18n-js-after" type="text/javascript">
-/* <![CDATA[ */
-wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
-/* ]]> */
-</script>
-<script id="password-strength-meter-js-extra" type="text/javascript">
-/* <![CDATA[ */
-var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
-/* ]]> */
-</script>
-<script id="password-strength-meter-js" src="wp-admin/js/password-strength-meter.min.js__querystring__ver=6.6.1" type="text/javascript"></script>
-<script id="underscore-js" src="wp-includes/js/underscore.min.js__querystring__ver=1.13.4" type="text/javascript"></script>
-<script id="wp-util-js-extra" type="text/javascript">
-/* <![CDATA[ */
-var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
-/* ]]> */
-</script>
-<script id="wp-util-js" src="wp-includes/js/wp-util.min.js__querystring__ver=6.6.1" type="text/javascript"></script>
-<script id="user-profile-js-extra" type="text/javascript">
-/* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"ef3b826a07"};
-/* ]]> */
-</script>
-<script id="user-profile-js" src="wp-admin/js/user-profile.min.js__querystring__ver=6.6.1" type="text/javascript"></script>
 	
 	
 	</body></html>
